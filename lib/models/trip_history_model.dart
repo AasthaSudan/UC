77a8266f51be1fdfd363ebf1b3584fc9d7b15847
Aastha Sudan <aasthasudan469@gmail.com>
2026-01-20 00:01:13 +0530

    new25

--- lib/models/trip_history_model.dart
+++ lib/models/trip_history_model.dart
@@ -1,32 +1,32 @@
-// class TripHistoryModel {
-//   String? time;
-//   String? originAddress;
-//   String? destinationAddress;
-//   String? status;
-//   String? fareAmount;
-//   String? driverName;
-//   String? car_details;
-//   String? ratings;
-//
-//   TripHistoryModel({
-//     this.time,
-//     this.originAddress,
-//     this.destinationAddress,
-//     this.status,
-//     this.fareAmount,
-//     this.driverName,
-//     this.car_details,
-//     this.ratings,
-// });
-//
-//   TripHistoryModel.fromSnapshot(DataSnapshot snapshot) {
-//     time=(snapshot.value as Map)["time"];
-//     originAddress=(snapshot.value as Map)["originAddress"];
-//     destinationAddress=(snapshot.value as Map)["destinationAddress"];
-//     status=(snapshot.value as Map)["status"];
-//     fareAmount=(snapshot.value as Map)["fareAmount"];
-//     driverName=(snapshot.value as Map)["driverName"];
-//     car_details=(snapshot.value as Map)["car_details"];
-//     ratings=(snapshot.value as Map)["ratings"];
-//   }
-// }
+class TripHistoryModel {
+  String? time;
+  String? originAddress;
+  String? destinationAddress;
+  String? status;
+  String? fareAmount;
+  String? driverName;
+  String? car_details;
+  String? ratings;
+
+  TripHistoryModel({
+    this.time,
+    this.originAddress,
+    this.destinationAddress,
+    this.status,
+    this.fareAmount,
+    this.driverName,
+    this.car_details,
+    this.ratings,
+});
+
+  TripHistoryModel.fromSnapshot(DataSnapshot snapshot) {
+    time=(snapshot.value as Map)["time"];
+    originAddress=(snapshot.value as Map)["originAddress"];
+    destinationAddress=(snapshot.value as Map)["destinationAddress"];
+    status=(snapshot.value as Map)["status"];
+    fareAmount=(snapshot.value as Map)["fareAmount"];
+    driverName=(snapshot.value as Map)["driverName"];
+    car_details=(snapshot.value as Map)["car_details"];
+    ratings=(snapshot.value as Map)["ratings"];
+  }
+}
