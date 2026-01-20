8d4ae59ada29b14aa0d8417ea5e5aa0daf3395bb
Aastha Sudan <aasthasudan469@gmail.com>
2026-01-20 14:08:19 +0530

    new25

--- lib/widgets/progress_dialog.dart
+++ lib/widgets/progress_dialog.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ProgressDialog extends StatelessWidget {
-  const ProgressDialog({super.key});
+  final String? message;
+
+  const ProgressDialog({super.key, this.message});
 
   @override
   Widget build(BuildContext context) {
@@ -13,22 +15,24 @@ class ProgressDialog extends StatelessWidget {
           color: Colors.white,
           borderRadius: BorderRadius.circular(6),
         ),
-
-        child: Row(
-          children: [
-            SizedBox(width: 6),
-            CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-            ),
-            SizedBox(width: 26),
-            Text(
-              message!,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            children: [
+              SizedBox(width: 6),
+              CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
               ),
-            ),
-          ],
+              SizedBox(width: 26),
+              Text(
+                message ?? "Please wait...",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
