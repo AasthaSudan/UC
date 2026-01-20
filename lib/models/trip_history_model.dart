4e4f470415154e665bae0cd736fa8ea46d6ec962
Aastha Sudan <aasthasudan469@gmail.com>
2026-01-20 14:00:18 +0530

    new25

--- lib/models/trip_history_model.dart
+++ lib/models/trip_history_model.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_database/firebase_database.dart';
+
 class TripHistoryModel {
   String? time;
   String? originAddress;
@@ -17,16 +19,16 @@ class TripHistoryModel {
     this.driverName,
     this.car_details,
     this.ratings,
-});
+  });
 
   TripHistoryModel.fromSnapshot(DataSnapshot snapshot) {
-    time=(snapshot.value as Map)["time"];
-    originAddress=(snapshot.value as Map)["originAddress"];
-    destinationAddress=(snapshot.value as Map)["destinationAddress"];
-    status=(snapshot.value as Map)["status"];
-    fareAmount=(snapshot.value as Map)["fareAmount"];
-    driverName=(snapshot.value as Map)["driverName"];
-    car_details=(snapshot.value as Map)["car_details"];
-    ratings=(snapshot.value as Map)["ratings"];
+    time = (snapshot.value as Map)["time"];
+    originAddress = (snapshot.value as Map)["originAddress"];
+    destinationAddress = (snapshot.value as Map)["destinationAddress"];
+    status = (snapshot.value as Map)["status"];
+    fareAmount = (snapshot.value as Map)["fareAmount"];
+    driverName = (snapshot.value as Map)["driverName"];
+    car_details = (snapshot.value as Map)["car_details"];
+    ratings = (snapshot.value as Map)["ratings"];
   }
 }
